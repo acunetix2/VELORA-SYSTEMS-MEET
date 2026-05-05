ac8eb9bddb05c32ebbd50fc5b435182283be5008
phantomjac <drchesireyang@gmail.com>
2026-05-05 12:04:13 +0300

    Feature :Ready for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import netlify from '@netlify/vite-plugin-tanstack-start' 
-import { tanstackStart } from '@tanstack/start/vite'
 
 export default defineConfig({
-  plugins: [
-    netlify(), // ← add this (anywhere in the array is fine)
-    viteReact(),
-  ],
-})
+  // no manual plugins needed
+});
